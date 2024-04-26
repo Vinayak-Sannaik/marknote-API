d7aabf7e0501cbd0e03418b996654f1fb7c1a180
Vinayak-Sannaik <vinayakpsannaik@gmail.com>
2024-04-26 20:24:33 +0530

    Test Deployement 2.0

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,8 +1,10 @@
 const allowedOrigin = [
-    'http://localhost:3000',
-    'https://www.vinflyer.com',
-    'https://vinflyer.com',
-    'http://localhost:5174'
+    // 'http://localhost:3000',
+    // 'https://www.vinflyer.com',
+    // 'https://vinflyer.com',
+    // 'http://localhost:5174',
+    
+    'https://marknotes-ten.vercel.app/'
 
 ]
 
