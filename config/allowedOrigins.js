7fe3eba2492b4e49e619ba50e8d0369482b026a4
Vinayak-Sannaik <vinayakpsannaik@gmail.com>
2024-04-26 22:34:02 +0530

    Test Deployement 3.0

---
+++ config/allowedOrigins.js
@@ -0,0 +1,13 @@
+const allowedOrigins = [
+    "http://localhost:5174", 
+    "http://localhost:3000",
+    "https://marknotes-ten.vercel.app"
+];
+
+module.exports = allowedOrigins;
+
+// ,
+// 'https://www.vinflyer.com',
+// 'https://vinflyer.com',
+
+// https://marknotes-ten.vercel.app/
--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,15 +1,15 @@
-const allowedOrigin = require('./allowedOrigin')
+const allowedOrigins = require('./allowedOrigins')
 
 const corsOptions = {
     origin : (origin, callback) => {
-        if(allowedOrigin.indexOf(origin) !== -1 || !origin){
+        if(allowedOrigins.indexOf(origin) !== -1 || !origin){
             callback(null, true)
         } else{
             callback(new Error('Not allowed by CORS'))
         }
     },
-    credentials : true,
-    optionSuccessStatus : 200
+    credentials: true,
+    optionsSuccessStatus: 200
 }
 
 module.exports = corsOptions
