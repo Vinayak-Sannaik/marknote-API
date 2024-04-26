b40c90fc87e6929d9e1364ef67d2d74cad0edd15
Vinayak Sannaik <112576218+Vinayak-Sannaik@users.noreply.github.com>
2024-04-26 21:02:40 +0530

    Update allowedOrigin.js

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,11 +1,5 @@
 const allowedOrigin = [
-    // 'http://localhost:3000',
-    // 'https://www.vinflyer.com',
-    // 'https://vinflyer.com',
-    // 'http://localhost:5174',
-    
     'https://marknotes-ten.vercel.app/'
-
 ]
 
 module.exports = allowedOrigin
