197f5bab20f8e3457a21e56c2219aae145c8624b
Vinayak-Sannaik <vinayakpsannaik@gmail.com>
2024-04-27 07:32:45 +0530

    Test Deployement 4.3 rate limiter setup

--- middleware/loginLimiter.js
+++ middleware/loginLimiter.js
@@ -1,17 +1,15 @@
-const rateLimit = require('express-rate-limit')
-const { logEvents } = require('./logger')
+const rateLimit = require('express-rate-limit');
+const { logEvents } = require('./logger');
 
 const loginLimiter = rateLimit({
     windowMs: 60 * 1000, // 1 minute
-    max: 5, // Limit each IP to 5 login requests per `window` per minute
-    message:
-        { message: 'Too many login attempts from this IP, please try again after a 60 second pause' },
+    max: 5, // Limit each IP to 5 login requests per minute
+    message: 'Too many login attempts from this IP, please try again after a 60 second pause',
     handler: (req, res, next, options) => {
-        logEvents(`Too Many Requests: ${options.message.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log')
-        res.status(options.statusCode).send(options.message)
+        logEvents(`Too Many Requests: ${options.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log');
+        res.status(options.statusCode).send({ message: options.message });
     },
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-})
+    headers: true, // Include rate limit info in the response headers
+});
 
-module.exports = loginLimiter
+module.exports = loginLimiter;
